9e9099b7387b18b710a944b7ab36ccf4d1d39e35
Disney Javiya <PCE88@server7.com>
2024-02-05 16:31:29 +0530

    Patient module

--- HalloDoc/wwwroot/js/custom.js
+++ HalloDoc/wwwroot/js/custom.js
@@ -1,36 +1,23 @@
-function darkMode() {
-    var element = document.querySelector(".patient-login-main");
-   
-    element.classList.toggle("dark-mode");
-   
-}
 const eyeIcon = document.querySelector('.eye-icon');
+const eyeIcon1 = document.querySelector('.eye-icon1');
 const password = document.querySelector('.password');
-eyeIcon.addEventListener("click", function(){
+const password1 = document.querySelector('.password1');
+eyeIcon.addEventListener("click", function () {
     const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-            
-            // toggle the icon
-            this.classList.toggle("fa-regular fa-eye");
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("fa-regular fa-eye");
 });
-function darkModeReset() {
-    var element = document.querySelector(".patient-reset-main");
-   
-    element.classList.toggle("dark-mode");
-   
-}
-function darkModePatientSitePage(){
-     var element1 = document.querySelector(".patient-site-main");
-     element1.classList.toggle("dark-mode");
-}
+eyeIcon1.addEventListener("click", function () {
+    const type1 = password1.getAttribute("type") === "password" ? "text" : "password";
+    password1.setAttribute("type", type1);
 
-function darkModePatientRequestPage(){
-    var element1 = document.querySelector(".patient-submit-main");
-    element1.classList.toggle("dark-mode");
-}
-
-function darkModeCreatePatientRequestPage(){
-    var element1 = document.querySelector(".create-patient-req-main");
+    // toggle the icon
+    this.classList.toggle("fa-regular fa-eye");
+});
+function darkMode() {
+    var element1 = document.querySelector(".full-content");
     element1.classList.toggle("dark-mode");
 }
 
